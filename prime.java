fac823ee24cb72fd36166fb54fcf3c94544a37dd
lavanya938 <lavimukkapati1@gmail.com>
2024-11-27 14:30:10 +0530

    first commit

---
+++ prime.java
@@ -0,0 +1,25 @@
+
+public class prime {
+
+    public static void main(String[] args) {
+
+        int num = 26;
+
+        System.out.println(isPrime(num) ? "Prime" : "Not Prime");
+
+    }
+
+    static boolean isPrime(int n) {
+
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i <= n / 2; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+
+    }
+
+}
